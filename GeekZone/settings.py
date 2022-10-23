6fd13b56c773dbd4a23b79a9edebba7e204cc8a8
Shahnaouj Alam Rohan <shahnaouj517@gmail.com>
2022-10-24 02:04:29 +0600

    data entry

--- GeekZone/settings.py
+++ GeekZone/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #MEDIA_URL = 'images'
 
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
